e846c5b1a579cc29f94cd24d154091ffc3c89fa2
Siddharth Jain <siddharthj1405@gmail.com>
2024-03-20 02:12:01 +0530

    About Section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./About.css";
+import play_icon from "../../assets/play-icon.png";
+import about from "../../assets/about.jpg";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left">
+        <img src={about} className="about-img"></img>
+        <img src={play_icon} className="play-icon"></img>
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Empowering Minds, Inspiring Futures</h2>
+        <p>
+          VIT was established with the aim of providing quality higher education
+          on par with international standards. It persistently seeks and adopts
+          innovative methods to improve the quality of higher education on a
+          consistent basis. The campus has a cosmopolitan atmosphere with
+          students from all corners of the globe.
+        </p>
+        <p>
+          Experienced and learned teachers are strongly encouraged to nurture
+          the students. The global standards set at VIT in the field of teaching
+          and research spur us on in our relentless pursuit of excellence. In
+          fact, it has become a way of life for us.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;

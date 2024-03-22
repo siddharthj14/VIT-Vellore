91727715527de2b259f1a89bd5ebd6a3449fd451
Siddharth Jain <siddharthj1405@gmail.com>
2024-03-22 15:28:59 +0530

    Contact section

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "1ca29ebb-3e98-4dbd-8f6b-0e812b1fe675");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} />
+            Contact@GreatStack.dev
+          </li>
+          <li>
+            <img src={phone_icon} />
+            +1 123-456-7890
+          </li>
+          <li>
+            <img src={location_icon} />
+            77 Massachusetts Ave, Cambridge
+            <br /> MA 02139, United States
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Your Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email id"
+            required
+          />
+          <label>Write your messages here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now →
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

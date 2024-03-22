195ec8d2e008d2612738441993f74d058572a4bf
Siddharth Jain <siddharthj1405@gmail.com>
2024-03-22 16:47:34 +0530

    Navbar links working

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [style, setStyle] = useState(false);
@@ -15,13 +16,37 @@ const Navbar = () => {
     <nav className={"container " + (style && "nav-bg")}>
       <img src={logo} alt="VIT Logo" className="logo"></img>
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className="btn">Contact us</button>
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="programs" smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={-150} duration={500}>
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <button className="btn">
+            <Link to="contact" smooth={true} offset={-260} duration={500}>
+              Contact us
+            </Link>
+          </button>
         </li>
       </ul>
     </nav>
